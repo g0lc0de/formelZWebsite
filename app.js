ef99a9a19b60b50b9d68e0ebd432cb16dbb121c7
Niklas <niklash187@gmail.com>
2020-12-06 22:28:36 +0100

    improvements for mobiles and tablets

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
 
-app.set("view engine","ejs")
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine","ejs");
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req,res) => {
     res.render("Informationen.ejs")
