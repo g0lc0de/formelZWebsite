430494b2db2a239741c10dda3be93b5b2bbac836
Niklas <niklash187@gmail.com>
2020-12-06 04:57:10 +0100

    Boilerplate without any style

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000 || process.env.PORT;
+
+app.set("view engine","ejs")
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.get("/", (req,res) => {
+    res.render("Informationen.ejs")
+});
+
+app.listen(port, () => {
+    console.log(`App listening now on port ${port}`);
+});
